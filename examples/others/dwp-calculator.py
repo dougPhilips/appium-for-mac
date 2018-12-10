78b2201e53256d4da77a194bb3b024b5a7950dc7
doug9425 <Doug.Philips@rackspace.com>
2018-12-09 21:36:10 -0500

    CHECKPOINT

--- examples/others/dwp-calculator.py
+++ examples/others/dwp-calculator.py
@@ -21,7 +21,7 @@ programmerGroupPath = windowPath + "/AXGroup[1]"
 print 'Starting the WebDriver session'
 defaultCookies = [
     {'name': 'loop_delay', 'value': 1.00},
-    {'name': 'command_delay', 'value': 0.100},
+    {'name': 'command_delay', 'value': 0.500},
     {'name': 'implicit_timeout', 'value': 0.100},
     {'name': 'mouse_speed', 'value': 100},
     {'name': 'screen_shot_on_error', 'value': False},
@@ -103,6 +103,7 @@ def clickElement(element):
 def do_some_calculations_with_clicks():
     print 'Clearing the calculator'
     clickElement(button_clear)
+    clickElement(button_clear)
 
     rand1 = randint(0, 1000)
     rand2 = randint(0, 1000)
@@ -134,9 +135,49 @@ def do_some_calculations_with_clicks():
         print 'Incorect Result:', answer, 'Should be:', rand1 + rand2
 
 
+def do_scientific_calcs():
+    print 'Clearing the calculator'
+    ActionChains(driver).send_keys('cc').perform()
+
+    result = text_result.text
+    if result != '0':
+        print
+        print "Expect 0, got:", result
+
+
+    pie_button.click()
+    result = text_result.text
+    if not result.startswith('3.14159'):
+        print
+        print "Expected pi, got:", result
+
+    ActionChains(driver).send_keys('c').perform()
+    ActionChains(driver).send_keys('5').perform()
+    factorial_button.click()
+    result = text_result.text
+    if result != '120':
+        print
+        print 'Expected 120, got:', result
+
+    ActionChains(driver).send_keys('cc').perform()
+    # ActionChains(driver).key_up(Keys.SHIFT).send_keys('9').key_down(Keys.SHIFT).send_keys('2+3').key_up(Keys.SHIFT).send_keys('0').key_down(Keys.SHIFT).send_keys('*5=').perform()
+    ActionChains(driver).send_keys('(2+3)*5=').perform()
+    result = text_result.text
+    if result != '25':
+        print
+        print 'expected 25, got:', result
+
+    ActionChains(driver).send_keys('c').perform()
+    ActionChains(driver).send_keys('2+3*5=').perform()
+    result = text_result.text
+    if result != '17':
+        print
+        print 'expected 17, got:', result
+
+
 def do_some_calculations_with_keystrokes():
     print 'Clearing the calculator'
-    ActionChains(driver).send_keys('c').perform()
+    ActionChains(driver).send_keys('cc').perform()
 
     rand1 = randint(0, 1000)
     rand2 = randint(0, 1000)
@@ -171,28 +212,27 @@ def calculator_mode(mode):
 
 
 view_menu_xpath = "/AXApplication/AXMenuBar/AXMenuBarItem[@AXTitle='View']"
-driver.find_element_by_xpath(view_menu_xpath).click()
-driver.find_element_by_xpath(calculator_mode('Basic')).click()
-sleep(2)
-driver.find_element_by_xpath(view_menu_xpath).click()
-driver.find_element_by_xpath(calculator_mode('Scientific')).click()
-sleep(2)
-driver.find_element_by_xpath(view_menu_xpath).click()
-driver.find_element_by_xpath(calculator_mode('Programmer')).click()
-sleep(2)
+# driver.find_element_by_xpath(view_menu_xpath).click()
+# driver.find_element_by_xpath(calculator_mode('Basic')).click()
+# sleep(2)
+# driver.find_element_by_xpath(view_menu_xpath).click()
+# driver.find_element_by_xpath(calculator_mode('Scientific')).click()
+# sleep(2)
+# driver.find_element_by_xpath(view_menu_xpath).click()
+# driver.find_element_by_xpath(calculator_mode('Programmer')).click()
+# sleep(2)
 
 print "Now by keys!"
 
 # Command key is a toggle/shift
 
-ActionChains(driver).key_up(Keys.COMMAND).send_keys("1").key_down(Keys.COMMAND).perform()
-sleep(2)
+# ActionChains(driver).key_up(Keys.COMMAND).send_keys("1").key_down(Keys.COMMAND).perform()
+# sleep(2)
+# ActionChains(driver).key_up(Keys.COMMAND).send_keys("2").key_down(Keys.COMMAND).perform()
+# sleep(2)
+# ActionChains(driver).key_up(Keys.COMMAND).send_keys("3").key_down(Keys.COMMAND).perform()
+# sleep(2)
 ActionChains(driver).key_up(Keys.COMMAND).send_keys("2").key_down(Keys.COMMAND).perform()
-sleep(2)
-ActionChains(driver).key_up(Keys.COMMAND).send_keys("3").key_down(Keys.COMMAND).perform()
-sleep(2)
-ActionChains(driver).key_up(Keys.COMMAND).send_keys("1").key_down(Keys.COMMAND).perform()
-sleep(2)
 
 button_clear = driver.find_element_by_xpath(basicGroupPath + "/AXButton[@AXDescription='clear']")
 button_plus = driver.find_element_by_xpath(basicGroupPath + "/AXButton[@AXDescription='add']")
@@ -204,26 +244,31 @@ quit_menu_item_xpath = "/AXApplication/AXMenuBar/AXMenuBarItem/AXMenu/" + \
                        "AXMenuItem[@AXTitle='Quit Calculator']"
 quit_menu_item = driver.find_element_by_xpath(quit_menu_item_xpath)
 
-print
-print
-print 'Doing calculations with native mouse events...'
+pie_button = driver.find_element_by_id('_NS:307')
+factorial_button = driver.find_element_by_id('_NS:297')
 
-useNativeEvents = 1
-do_some_calculations_with_clicks()
+# print
+# print
+# print 'Doing calculations with native mouse events...'
 
-print
-print
-print 'Doing calculations with accessibility actions...'
+# useNativeEvents = 1
+# do_some_calculations_with_clicks()
 
-useNativeEvents = 0
-do_some_calculations_with_clicks()
+# print
+# print
+# print 'Doing calculations with accessibility actions...'
 
-print
-print
-useNativeEvents = 1
-print 'Doing calculations with native keystrokes...'
+# useNativeEvents = 0
+# do_some_calculations_with_clicks()
 
-do_some_calculations_with_keystrokes()
+# print
+# print
+# useNativeEvents = 1
+# print 'Doing calculations with native keystrokes...'
+
+# do_some_calculations_with_keystrokes()
+
+do_scientific_calcs()
 
 # quit the webdriver instance
 print 'Quitting the WebDriver session'
@@ -233,3 +278,38 @@ print 'Quitting the WebDriver session'
 # driver.execute('closeApp')
 ActionChains(driver).send_keys(Keys.COMMAND + "q").perform()
 driver.quit()
+
+
+
+#################
+
+# <AXButton AXDescription="right paren" AXIdentifier="_NS:140">
+# <AXButton AXDescription="left paren" AXIdentifier="_NS:150">
+# <AXButton AXDescription="inverse" AXIdentifier="_NS:160">
+# <AXButton AXDescription="sine" AXIdentifier="_NS:170">
+# <AXButton AXDescription="hyperbolic cosine" AXIdentifier="_NS:179">
+# <AXButton AXDescription="x squared" AXIdentifier="_NS:188">
+# <AXButton AXDescription="square root" AXIdentifier="_NS:197">
+# <AXButton AXDescription="cube root" AXIdentifier="_NS:206">
+# <AXButton AXDescription="nth root" AXIdentifier="_NS:215">
+# <AXButton AXDescription="hyperbolic sine" AXIdentifier="_NS:224">
+# <AXButton AXDescription="x to the power of y" AXIdentifier="_NS:233">
+# <AXButton AXDescription="x cubed" AXIdentifier="_NS:242">
+# <AXButton AXDescription="natural logarithm" AXIdentifier="_NS:251">
+# <AXButton AXDescription="log base 10" AXIdentifier="_NS:261">
+# <AXButton AXDescription="tangent" AXIdentifier="_NS:270">
+# <AXButton AXDescription="hyperbolic tangent" AXIdentifier="_NS:279">
+# <AXButton AXDescription="cosine" AXIdentifier="_NS:288">
+# <AXButton AXDescription="x factorial" AXIdentifier="_NS:297">
+# <AXButton AXDescription="pi" AXIdentifier="_NS:307">
+# <AXButton AXDescription="random number" AXIdentifier="_NS:317">
+# <AXButton AXDescription="e" AXIdentifier="_NS:326">
+# <AXButton AXDescription="EE" AXIdentifier="_NS:333">
+# <AXButton AXDescription="radians" AXIdentifier="_NS:343">
+# <AXButton AXDescription="10 to the x" AXIdentifier="_NS:351">
+# <AXButton AXDescription="secondary functions" AXIdentifier="_NS:357">
+# <AXButton AXDescription="memory clear" AXIdentifier="_NS:366">
+# <AXButton AXDescription="memory plus" AXIdentifier="_NS:375">
+# <AXButton AXDescription="memory minus" AXIdentifier="_NS:384">
+# <AXButton AXDescription="memory recall" AXIdentifier="_NS:393">
+# <AXButton AXDescription="e to the x" AXIdentifier="_NS:402">
